ba60f6f52281dfe571dae1c06560d40d48e38615
Manuel Gutierrez Moya <manuel.gutierrez.moya@nttdata.com>
2023-05-15 23:51:16 +0200

    [WIP] Add StatteFlow and calls to usecases from viewModel

--- app/src/main/java/com/manudev/finalcomposeapp/addtasks/ui/TasksViewModel.kt
+++ app/src/main/java/com/manudev/finalcomposeapp/addtasks/ui/TasksViewModel.kt
@@ -1,16 +1,34 @@
 package com.manudev.finalcomposeapp.addtasks.ui
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.manudev.finalcomposeapp.addtasks.domain.AddTaskUseCase
+import com.manudev.finalcomposeapp.addtasks.domain.GetTasksUseCase
+import com.manudev.finalcomposeapp.addtasks.ui.TasksUiState.Error
+import com.manudev.finalcomposeapp.addtasks.ui.TasksUiState.Loading
+import com.manudev.finalcomposeapp.addtasks.ui.TasksUiState.Success
 import com.manudev.finalcomposeapp.addtasks.ui.model.TaskModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class TasksViewModel @Inject constructor() : ViewModel() {
+class TasksViewModel @Inject constructor(
+    private val addTaskUseCase: AddTaskUseCase,
+    getTasksUseCase: GetTasksUseCase
+) : ViewModel() {
+
+    val uiState: StateFlow<TasksUiState> = getTasksUseCase().map(::Success)
+        .catch { Error(it) }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), Loading)
 
 
     private val _showDialog = MutableLiveData<Boolean>()
@@ -26,7 +44,12 @@ class TasksViewModel @Inject constructor() : ViewModel() {
     fun onTaskCreated(task: String) {
         _showDialog.value = false
         _taskList.add(TaskModel(task = task))
-        Log.i("ManuDev", task)
+
+        viewModelScope.launch {
+            addTaskUseCase.invoke(
+                TaskModel(task = task)
+            )
+        }
 
     }
 
